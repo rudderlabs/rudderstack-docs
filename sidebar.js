f5fb3120156afdd8984a8230e22555113d8b2e73
Amey Varangaonkar <59817155+ameypv-rudder@users.noreply.github.com>
2021-10-29 13:09:10 +0530

    Update sidebar.js

--- sidebar.js
+++ sidebar.js
@@ -1841,14 +1841,6 @@ export const jsonData = [
            ]
         },
         {
-           "key":"test-api",
-           "title":"Test API",
-           "link":"/rudderstack-api/test-api/",
-           "content":[
-              
-           ]
-        },
-        {
            "key":"pixel-api-spec",
            "title":"Pixel API",
            "link":"/rudderstack-api/pixel-api-spec/",
