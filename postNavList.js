be29dff24f7202aa0affd36b9935ed2de21a98aa
Amey Varangaonkar <59817155+ameypv-rudder@users.noreply.github.com>
2021-11-02 16:32:31 +0530

    Update postNavList.js

--- postNavList.js
+++ postNavList.js
@@ -117,11 +117,35 @@ export const postNavList = [
     "content":[]
  },
  {
+    "key":"quick-start-guide",
+    "title":"Quick Start Guide",
+    "link":"/stream-sources/rudderstack-sdk-integration-guides/rudderstack-javascript-sdk/quick-start-guide/",
+    "content":[]
+ },
+ {
     "key":"javascript-sdk-enhancements",
     "title":"JavaScript SDK Enhancements",
     "link":"/stream-sources/rudderstack-sdk-integration-guides/rudderstack-javascript-sdk/javascript-sdk-enhancements/",
     "content":[]
  },
+ {
+    "key":"querystring-api",
+    "title":"Querystring API",
+    "link":"/stream-sources/rudderstack-sdk-integration-guides/rudderstack-javascript-sdk/querystring-api/",
+    "content":[]
+ },
+ {
+    "key":"detecting-adblocked-pages",
+    "title":"Detecting Ad-blocked Pages",
+    "link":"/stream-sources/rudderstack-sdk-integration-guides/rudderstack-javascript-sdk/detecting-adblocked-pages/",
+    "content":[]
+ },
+ {
+    "key":"js-sdk-faqs",
+    "title":"FAQs",
+    "link":"/stream-sources/rudderstack-sdk-integration-guides/rudderstack-javascript-sdk/js-sdk-faqs/",
+    "content":[]
+ }, 
  {
     "key":"rudderstack-android-sdk",
     "title":"Android",
