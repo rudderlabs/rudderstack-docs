d5efb4a5bd638c2f2cc44c047ec13e1bc9ce728d
Amey Varangaonkar <59817155+ameypv-rudder@users.noreply.github.com>
2021-11-19 23:49:47 +0530

    Update postNavList.js

--- postNavList.js
+++ postNavList.js
@@ -418,12 +418,6 @@ export const postNavList = [
       "link":"/cloud-extract-sources/zendesk-chat/",
       "content":[]
    },
-   {
-    "key":"intercom-v2",
-    "title":"Intercom v2",
-    "link":"/cloud-extract-sources/intercom-v2/",
-    "content":[]
-   },
  {
     "key":"warehouse-actions",
     "title":"Warehouse Actions Sources",
