2a81edbd11b0846c8cb12c5ccae24b28932fb32f
ishachopra0401 <97496803+ishachopra0401@users.noreply.github.com>
2022-04-22 18:29:26 +0530

    Cross reference links for RS Cloud (#521)
    * Update glossary.mdx
    
    * Updated links
    
    * Getting started cross-references updated
    
    * Updated links
    
    * updated links
    
    * updated source and destination links
    
    * Update pinterest-ads.mdx
    
    * updated links
    
    * Delete adding-source-and-destination-rudderstack.mdx
    
    * Delete index.mdx
    
    * Update rudderstack-react-native-sdk.mdx
    
    * Update test-api.mdx
    
    * Update http-api.mdx
    
    * Update splitio.mdx
    
    * Update optimizely-web.mdx
    
    * Update amazon-eventbridge.mdx
    
    * Update marketo.mdx
    
    * Update kubit.mdx
    
    * Removed references
    
    * Doc updates
    
    Co-authored-by: Amey Varangaonkar <amey@rudderlabs.com>

--- postNavList.js
+++ postNavList.js
@@ -1757,12 +1757,6 @@ export const postNavList = [
     "content":[]
  },
  {
-   "key":"architecture",
-   "title":"Architecture",
-   "link":"/get-started/rudderstack-architecture/",
-   "content":[]
- },
- {
     "key":"user-guides",
     "title":"User Guides",
     "link":"/user-guides/",
