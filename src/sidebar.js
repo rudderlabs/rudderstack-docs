c0c04c5815051ee042f0e935022b99ef46b9750d
Maxim Sadovsky <maksim@sadovskij.com>
2022-05-16 12:05:53 +0300

    Move to /docs

--- src/sidebar.js
+++ src/sidebar.js
@@ -1140,6 +1140,12 @@ export const jsonData = [
             content: [],
           },
           {
+            key: "attribution",
+            title: "Attribution",
+            link: "/destinations/attribution/attribution/",
+            content: [],
+          },
+          {
             key: "branchio",
             title: "Branch",
             link: "/destinations/attribution/branchio/",
