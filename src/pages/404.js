c73f2bb876ab4d0c7318f890f9213ba5ced12400
Gauravp15 <gauravprabhu15@gmail.com>
2022-06-03 20:36:36 +0530

    Fixed classname

--- src/pages/404.js
+++ src/pages/404.js
@@ -7,7 +7,7 @@ const Banner404 = props => {
     <div className="mx-auto banner-404 pb-12 lg:pb-80">
         <DocsNavigation />
       <section className="overflow-auto ">
-        <div class="max-w-6xl mt-20 md:mt-40 px-3 md:px-3 mx-auto flex flex-wrap flex-col md:items-center md:text-center">
+        <div className="max-w-6xl mt-20 md:mt-40 px-3 md:px-3 mx-auto flex flex-wrap flex-col md:items-center md:text-center">
           <h1 className="text-9xl md:text-20xl md:-mt-16 font-bold font-custom leading-snug text-blueNew-midnight text-center">
             404
           </h1>
