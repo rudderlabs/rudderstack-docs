ccaf001308cefc89818f912857a5834be194c78e
pfd <pdavis@rudderstack.com>
2022-07-18 09:21:48 -0700

    div

--- src/@rocketseat/gatsby-theme-docs/components/Layout/index.js
+++ src/@rocketseat/gatsby-theme-docs/components/Layout/index.js
@@ -104,10 +104,11 @@ export default function Layout({ children, title, headings, description }) {
                 </div>
               )}
               {children}
+              <div>
               <hr></hr>
               <h2>Contact us</h2><p>
                 For more information on the topics covered on this page, <a href="mailto:docs@rudderstack.com">email us</a> or start a conversation in our <a href="https://rudderstack.com/join-rudderstack-slack-community">Slack community</a>.</p>
-                <hr></hr>
+              </div>
             </div>
           </Children>
           <div
