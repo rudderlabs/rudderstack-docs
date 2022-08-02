4a445f557b8cc61fd4bddd69da06d647d2822c93
Gerlando Piro <gerlando@gmail.com>
2022-08-02 15:06:11 -0700

    Prettier & ESLint applied

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,10 +1,10 @@
 if (!process.env.GATSBY_SANITY_TOKEN) {
   exports.sourceNodes = ({ actions, createNodeId, createContentDigest }) => {
-    const { createNode } = actions;
-    const POST_NODE_TYPE = "SanitySiteSettings";
+    const { createNode } = actions
+    const POST_NODE_TYPE = 'SanitySiteSettings'
     const post = {
-      _rawWebsiteScripts: ["test"]
-    };
+      _rawWebsiteScripts: ['test'],
+    }
 
     createNode({
       ...post,
@@ -13,26 +13,26 @@ if (!process.env.GATSBY_SANITY_TOKEN) {
       children: [],
       internal: {
         type: POST_NODE_TYPE,
-        contentDigest: createContentDigest(post)
-      }
-    });
-  };
+        contentDigest: createContentDigest(post),
+      },
+    })
+  }
 }
 
 exports.onPostBuild = () => {
-  const fs = require("fs");
+  const fs = require('fs')
 
   /*
    * If sitemap exists in /public/sitemaps folder, move sitemap-0.xml
    * to root folder (/public) & rename to sitemap-docs.xml
    */
-  if (fs.existsSync("./public/sitemaps/sitemap-0.xml")) {
+  if (fs.existsSync('./public/sitemaps/sitemap-0.xml')) {
     fs.renameSync(
-      "./public/sitemaps/sitemap-0.xml",
-      "./public/sitemap-docs.xml"
-    );
+      './public/sitemaps/sitemap-0.xml',
+      './public/sitemap-docs.xml'
+    )
 
     /* Remove the /public/sitemaps folder */
-    fs.rmSync("./public/sitemaps", { recursive: true, force: true });
+    fs.rmSync('./public/sitemaps', { recursive: true, force: true })
   }
-};
+}
--- src/@rocketseat/gatsby-theme-docs/components/Layout/index.js
+++ src/@rocketseat/gatsby-theme-docs/components/Layout/index.js
@@ -1,22 +1,22 @@
 /* @jsx jsx */
-import { useState, useRef, Fragment, useEffect } from "react";
-import { useStaticQuery, graphql } from "gatsby";
-import { jsx, css } from "@emotion/react";
-import PropTypes from "prop-types";
+import { useState, useRef, Fragment, useEffect } from 'react'
+import { useStaticQuery, graphql } from 'gatsby'
+import { jsx, css } from '@emotion/react'
+import PropTypes from 'prop-types'
 
-import TableOfContents from "../Docs/TOC/index";
-import Sidebar from "../Sidebar";
-import { Container, Main, Children } from "./styles";
-import DocsNavigation from "../../../../components/DocsNavigation";
-import tailwindConfig from "../../../../../tailwind.config";
-import "../../../../css/docs-critical.css";
-import "../../../../css/tailwind.css";
-import "../../../../css/docs.css";
-import "../../../../css/cookies.css";
+import TableOfContents from '../Docs/TOC/index'
+import Sidebar from '../Sidebar'
+import { Container, Main, Children } from './styles'
+import DocsNavigation from '../../../../components/DocsNavigation'
+import tailwindConfig from '../../../../../tailwind.config'
+import '../../../../css/docs-critical.css'
+import '../../../../css/tailwind.css'
+import '../../../../css/docs.css'
+import '../../../../css/cookies.css'
 
-import CookiesConsent from "../../../../components/cookiesConsent";
-import ScriptContentHead from "../../../../components/ScriptContentHead";
-import ScriptContentBody from "../../../../components/ScriptContentBody";
+import CookiesConsent from '../../../../components/cookiesConsent'
+import ScriptContentHead from '../../../../components/ScriptContentHead'
+import ScriptContentBody from '../../../../components/ScriptContentBody'
 
 export default function Layout({ children, title, headings, description }) {
   const data = useStaticQuery(graphql`
@@ -29,70 +29,70 @@ export default function Layout({ children, title, headings, description }) {
         }
       }
     }
-  `);
-  const contentRef = useRef(null);
-  const [isMenuOpen, setMenuOpen] = useState(false);
-  const [isTocOpen, setTocOpen] = useState(false);
-  const disableTOC = false;
+  `)
+  const contentRef = useRef(null)
+  const [isMenuOpen, setMenuOpen] = useState(false)
+  const [isTocOpen, setTocOpen] = useState(false)
+  const disableTOC = false
 
   const handleMenuOpen = () => {
-    setMenuOpen(!isMenuOpen);
-  };
+    setMenuOpen(!isMenuOpen)
+  }
 
   return (
     <Fragment>
       {data?.allSanitySiteSettings?.edges[0].node._rawWebsiteScripts
-        .filter((s) => s._key)
-        .map((script) => {
+        .filter(s => s._key)
+        .map(script => {
           return (
-            <ScriptContentHead key={script._key} currentSlug={""} {...script} />
-          );
+            <ScriptContentHead key={script._key} currentSlug={''} {...script} />
+          )
         })}
       <DocsNavigation handleMenuOpen={handleMenuOpen} isMenuOpen={isMenuOpen} />
       <Container>
-        <div className="sidebarWrapper">
+        <div className='sidebarWrapper'>
           <div
-            className={`sidebarOverlay ${isMenuOpen ? "active" : "hidden"}`}
+            className={`sidebarOverlay ${isMenuOpen ? 'active' : 'hidden'}`}
             onClick={() => handleMenuOpen()}
           ></div>
           <Sidebar isMenuOpen={isMenuOpen} />
         </div>
         <Main>
           <Children ref={contentRef}>
-            <div className="childrenWrapper">
-              <span className="tocMob" onClick={() => setTocOpen(!isTocOpen)}>
+            <div className='childrenWrapper'>
+              <span className='tocMob' onClick={() => setTocOpen(!isTocOpen)}>
                 <svg
-                  preserveAspectRatio="xMidYMid meet"
-                  height="1em"
-                  width="1em"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
+                  preserveAspectRatio='xMidYMid meet'
+                  height='1em'
+                  width='1em'
+                  fill='none'
+                  xmlns='http://www.w3.org/2000/svg'
+                  viewBox='0 0 24 24'
+                  strokeWidth='2'
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
                   stroke={
                     isTocOpen
                       ? tailwindConfig.theme.colors.blueNew.custom
                       : tailwindConfig.theme.colors.grayColor.docsText
                   }
-                  className={`tocIcon ${isTocOpen ? "active" : ""}`}
+                  className={`tocIcon ${isTocOpen ? 'active' : ''}`}
                 >
                   <g>
-                    <line x1="21" y1="10" x2="7" y2="10"></line>
-                    <line x1="21" y1="6" x2="3" y2="6"></line>
-                    <line x1="21" y1="14" x2="3" y2="14"></line>
-                    <line x1="21" y1="18" x2="7" y2="18"></line>
+                    <line x1='21' y1='10' x2='7' y2='10'></line>
+                    <line x1='21' y1='6' x2='3' y2='6'></line>
+                    <line x1='21' y1='14' x2='3' y2='14'></line>
+                    <line x1='21' y1='18' x2='7' y2='18'></line>
                   </g>
                 </svg>
               </span>
               {isTocOpen && (
-                <div className="mobTocWrapper">
+                <div className='mobTocWrapper'>
                   <div
-                    className={`tocOverlay ${isTocOpen ? "active" : "hidden"}`}
+                    className={`tocOverlay ${isTocOpen ? 'active' : 'hidden'}`}
                     onClick={() => setTocOpen(false)}
                   ></div>
-                  <div className="mobTocMenu">
+                  <div className='mobTocMenu'>
                     <TableOfContents
                       headings={headings}
                       disableTOC={disableTOC}
@@ -108,10 +108,10 @@ export default function Layout({ children, title, headings, description }) {
                 <hr />
                 <h2>Contact us</h2>
                 <p>
-                  For more information on the topics covered on this page,{" "}
-                  <a href="mailto:docs@rudderstack.com">email us</a> or start a
-                  conversation in our{" "}
-                  <a href="https://rudderstack.com/join-rudderstack-slack-community">
+                  For more information on the topics covered on this page,{' '}
+                  <a href='mailto:docs@rudderstack.com'>email us</a> or start a
+                  conversation in our{' '}
+                  <a href='https://rudderstack.com/join-rudderstack-slack-community'>
                     Slack community
                   </a>
                   .
@@ -121,7 +121,7 @@ export default function Layout({ children, title, headings, description }) {
           </Children>
           <div
             className={`table-outer ${
-              headings !== null && headings.length < 2 ? "hidden" : ""
+              headings !== null && headings.length < 2 ? 'hidden' : ''
             }`}
           >
             <TableOfContents
@@ -135,28 +135,28 @@ export default function Layout({ children, title, headings, description }) {
       </Container>
       <CookiesConsent />
       {data?.allSanitySiteSettings?.edges[0].node._rawWebsiteScripts
-        .filter((s) => s._key)
-        .map((script) => {
+        .filter(s => s._key)
+        .map(script => {
           return (
-            <ScriptContentBody key={script._key} currentSlug={""} {...script} />
-          );
+            <ScriptContentBody key={script._key} currentSlug={''} {...script} />
+          )
         })}
     </Fragment>
-  );
+  )
 }
 
 Layout.propTypes = {
   children: PropTypes.oneOfType([
     PropTypes.arrayOf(PropTypes.node),
-    PropTypes.node
+    PropTypes.node,
   ]).isRequired,
   disableTableOfContents: PropTypes.bool,
   title: PropTypes.string,
-  headings: PropTypes.array
-};
+  headings: PropTypes.array,
+}
 
 Layout.defaultProps = {
   disableTableOfContents: false,
-  title: "",
-  headings: null
-};
+  title: '',
+  headings: null,
+}
