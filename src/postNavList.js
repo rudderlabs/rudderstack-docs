50836cda810d83ffdde5f57794661046fd71776c
Amey Varangaonkar <59817155+ameypv-rudder@users.noreply.github.com>
2022-10-03 08:49:57 +0530

    Engage.so destination documentation (#918)
    * Engage.so destination documentation
    
    * Sidebar updates
    
    * Suggestions from doc review
    
    * Added FAQ
    
    * Apply suggestions from doc review
    
    Co-authored-by: Anant Jain <62471433+anantjain45823@users.noreply.github.com>
    
    * Apply suggestions from doc review
    
    Co-authored-by: Anant Jain <62471433+anantjain45823@users.noreply.github.com>
    
    * Apply suggestions from doc review
    
    Co-authored-by: ishachopra0401 <97496803+ishachopra0401@users.noreply.github.com>
    
    * Suggestions from doc review
    
    * Added a regulation example
    
    * Standardize section intro with other docs
    
    Co-authored-by: Anant Jain <62471433+anantjain45823@users.noreply.github.com>
    Co-authored-by: ishachopra0401 <97496803+ishachopra0401@users.noreply.github.com>

--- src/postNavList.js
+++ src/postNavList.js
@@ -1273,6 +1273,12 @@ export const postNavList = [
     link: "/destinations/streaming-destinations/user-com/",
     content: [],
   },
+  {
+    key: "engage",
+    title: "Engage",
+    link: "/destinations/streaming-destinations/engage/",
+    content: [],
+  },
    {
     key: "webengage",
     title: "WebEngage",
