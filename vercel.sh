0eb237493dbe1dd6748840f63d2d82a5ea374e0a
Gerlando Piro <gerlando@gmail.com>
2022-10-25 14:53:14 -0700

    Add echo statement for debugging builds

--- vercel.sh
+++ vercel.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
  
 if [[ $VERCEL_GIT_COMMIT_REF == "main"  ]] ; then 
+  echo 'build with prefix-paths'
   gatsby build --prefix-paths
 else 
+  echo 'build without prefix-paths'
   gatsby build
 fi
