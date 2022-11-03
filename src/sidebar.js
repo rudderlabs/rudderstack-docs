62487ca61523edd40f0cd1f55fad959288d9cfd9
ishachopra0401 <97496803+ishachopra0401@users.noreply.github.com>
2022-11-03 12:32:03 +0530

    updates

--- src/sidebar.js
+++ src/sidebar.js
@@ -631,8 +631,8 @@ export const jsonData = [
                 content: []
             },
             {
-                title: "Sending Event Data",
-                link: "/sources/reverse-etl/features/sending-event-data/",
+                title: "Syncing Events",
+                link: "/sources/reverse-etl/features/syncing-events/",
                 content: []
             },
             {
