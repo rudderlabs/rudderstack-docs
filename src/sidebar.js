db71da81cba5ec224b23b59a8a8f62107e3937bc
Amey Varangaonkar <amey@rudderlabs.com>
2022-12-06 11:25:55 +0530

    Google Sheets documentation

--- src/sidebar.js
+++ src/sidebar.js
@@ -492,7 +492,13 @@ export const jsonData = [
                 title: "Google Sheets",
                 link: "/sources/extract/google-sheets/",
                 content: []
-            }, {
+            },
+            {
+                title: "Google Sheets V2",
+                link: "/sources/extract/google-sheets-v2/",
+                content: []
+            }, 
+            {
                 title: "HubSpot",
                 link: "/sources/extract/hubspot-v2/",
                 content: []
