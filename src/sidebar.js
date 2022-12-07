cfff1ae6779dc725acd76c02045b378e4313e3b8
Amey Varangaonkar <59817155+ameypv-rudder@users.noreply.github.com>
2022-12-07 15:06:59 +0530

    Satismeter destination documentation (#1029)
    * Satismeter destination documentation
    
    * Added mappings for track
    
    * Settings updates
    
    * Updated destination name
    
    * Apply suggestions from doc review
    
    Co-authored-by: ishachopra0401 <97496803+ishachopra0401@users.noreply.github.com>
    
    * Apply suggestions from doc review
    
    * Apply suggestions from doc review
    
    Co-authored-by: ishachopra0401 <97496803+ishachopra0401@users.noreply.github.com>

--- src/sidebar.js
+++ src/sidebar.js
@@ -1663,6 +1663,11 @@ export const jsonData = [
                 ]
             },
             {
+                title: "SatisMeter",
+                link: "/destinations/streaming-destinations/satismeter/",
+                content: []
+            },
+            {
                 title: "WebEngage",
                 link: "/destinations/streaming-destinations/webengage/",
                 content: []
