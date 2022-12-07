bd1ae5d064cd9271868fa72560f12d5681038537
Amey Varangaonkar <59817155+ameypv-rudder@users.noreply.github.com>
2022-12-07 15:08:11 +0530

    Microsoft Clarity destination docs (#1024)
    * Microsoft Clarity destination docs
    
    * Apply suggestions from doc review
    
    Co-authored-by: ishachopra0401 <97496803+ishachopra0401@users.noreply.github.com>
    
    Co-authored-by: ishachopra0401 <97496803+ishachopra0401@users.noreply.github.com>

--- src/sidebar.js
+++ src/sidebar.js
@@ -1012,6 +1012,12 @@ export const jsonData = [
                 content: [],
             },
             {
+                key: "microsoft-clarity",
+                title: "Microsoft Clarity",
+                link: "/destinations/streaming-destinations/microsoft-clarity/",
+                content: [],
+            },
+            {
                 title: "Mixpanel",
                 link: "/destinations/streaming-destinations/mixpanel/",
                 content: []
