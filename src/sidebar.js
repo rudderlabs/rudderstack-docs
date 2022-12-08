b8d634304d8f7cc606999c8c126a37ebc13283e3
Amey Varangaonkar <amey@rudderlabs.com>
2022-12-08 11:54:54 +0530

    Revert "Google Analytics 4 source documentation"
    This reverts commit d10ce09940f9b4f55f1b53f53491dfe2f72682ec.

--- src/sidebar.js
+++ src/sidebar.js
@@ -482,11 +482,6 @@ export const jsonData = [
                 content: []
             }, 
             {
-                title: "Google Analytics 4",
-                link: "/sources/extract/google-analytics-4/",
-                content: []
-            },
-            {
                 title: "Google Analytics V2",
                 link: "/sources/extract/google-analytics-v2/",
                 content: []
