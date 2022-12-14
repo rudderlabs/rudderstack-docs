d89aac3ac6b7d243bab3ea9a11df52a3681a6aed
Amey Varangaonkar <amey@rudderlabs.com>
2022-12-14 13:15:44 +0530

    FTR update for warehouse destinations

--- src/@rocketseat/gatsby-theme-docs/components/Docs/index.js
+++ src/@rocketseat/gatsby-theme-docs/components/Docs/index.js
@@ -24,6 +24,7 @@ import YouTube from 'react-youtube'
 import Sources from '../../../../components/sources'
 import Destinations from '../../../../components/destinations'
 import Iplist from '../../../../components/iplist'
+import Ftrdestinations from '../../../../components/ftrdestinations'
 
 export default function Docs({mdx, pageContext}) {
   const {repositoryEditUrl, repositoryProvider} = pageContext
@@ -61,6 +62,7 @@ export default function Docs({mdx, pageContext}) {
     Iplist,
     Sources,
     Destinations,
+    Ftrdestinations,
     TabList,
     Tab,
     TabPanels,
