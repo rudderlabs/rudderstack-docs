af0511c8b023fb6d43b32ef6787e8398fc4ab505
pfd <pdavis@rudderstack.com>
2023-01-09 15:27:12 -0800

    update script

--- modified.py
+++ modified.py
@@ -1,15 +1,16 @@
 import os
 import subprocess
 
+# Iterate through the .mdx files in the docs folder and all subdirectories
 for root, dirs, files in os.walk("docs"):
     for file in files:
         if file.endswith(".mdx"):
             # Get the last time the file was modified
-            git_log_output = subprocess.run(["git", "log", "-1", "--format=%cd", file], capture_output=True)
+            git_log_output = subprocess.run(["git", "log", "-1", "--format=%cd", file], cwd=root, capture_output=True)
             last_modified = git_log_output.stdout.decode().strip()
             
             # Add the lastModified key/value pair to the frontmatter
-            with open(file, "r") as f:
+            with open(os.path.join(root, file), "r") as f:
                 content = f.read()
 
             # Check if the .mdx file contains two --- delimiters
@@ -18,7 +19,7 @@ for root, dirs, files in os.walk("docs"):
                 frontmatter += f"\nlastModified: {last_modified}\n"
 
                 # Write the modified content back to the file
-                with open(file, "w") as f:
+                with open(os.path.join(root, file), "w") as f:
                     f.write(f"---{frontmatter}---{rest}")
             else:
                 # Skip the file if it does not contain two --- delimiters
