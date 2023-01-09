3729a758bcc272fc89ba636774974bb127165cd4
pfd <pdavis@rudderstack.com>
2023-01-09 15:48:24 -0800

    update script

--- modified.py
+++ modified.py
@@ -1,35 +1,28 @@
 import os
 import subprocess
 
-# Iterate through the .mdx files in the docs folder and all subdirectories
-for root, dirs, files in os.walk("docs"):
-    for file in files:
-        if file.endswith(".mdx"):
-            # Get the last time the file was modified
-            git_log_output = subprocess.run(["git", "log", "-1", "--format=%cd", file], cwd=root, capture_output=True)
-            last_modified = git_log_output.stdout.decode().strip()
-            
-            # Add the lastModified key/value pair to the frontmatter
-            with open(os.path.join(root, file), "r") as f:
-                content = f.read()
+# Set the directory to the "docs" directory of the Gatsby repo
+docs_dir = "docs"
 
-
-                
-            # Find the index of the second line that starts with three dashes
-            end_index = 0
-            dashes_count = 0
-            for i, line in enumerate(lines):
-                if line.startswith('---'):
-                    dashes_count += 1
-                    if dashes_count == 2:
-                        end_index = i
-                        break
-            
-            # Insert the new lastModified key/value pair at the index right before the second line that starts with three dashes
-            lines.insert(end_index, f"lastModified: {last_modified}")
-            frontmatter = "\n".join(lines)
-
-            # Write the modified content back to the file
-            with open(os.path.join(root, file), "w") as f:
-                f.write(f"---{frontmatter}---{rest}")
-
+# Iterate through all the files in the docs directory
+for filename in os.listdir(docs_dir):
+  # Get the full path of the file
+  filepath = os.path.join(docs_dir, filename)
+  
+  # Check if the file is a Markdown file
+  if filepath.endswith(".mdx"):
+    # Open the file in read mode
+    with open(filepath, "r") as f:
+      # Read the entire file into a string
+      content = f.read()
+      
+    # Use git to get the last modified timestamp of the file
+    last_modified = subprocess.run(["git", "log", "-1", "--format=%ad", "--", filepath], capture_output=True).stdout.decode().strip()
+    
+    # Add the lastModified frontmatter item to the content
+    content = content.replace("---", "---\nlastModified: " + last_modified)
+    
+    # Open the file in write mode
+    with open(filepath, "w") as f:
+      # Write the modified content back to the file
+      f.write(content)
