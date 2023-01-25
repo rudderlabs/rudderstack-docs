d08727f18c37a2abf147eb397ee552ca7a6a6b91
ishachopra0401 <97496803+ishachopra0401@users.noreply.github.com>
2023-01-25 15:26:09 +0530

    Bing ads extract source doc (#1079)
    * cotent updates
    
    * bing ads doc
    
    * Update sidebar.js
    
    * content updates
    
    * Update bing-ads-v2-settings.png
    
    * Update bing-ads-v2.mdx

--- src/sidebar.js
+++ src/sidebar.js
@@ -475,6 +475,11 @@ export const jsonData = [
                 content: []
             },
             {
+                title: "Bing Ads V2",
+                link: "/sources/extract/bing-ads-v2/",
+                content: []
+            },
+            {
                 title: "Chargebee",
                 link: "/sources/extract/chargebee/",
                 content: []
